0e03c577c83aa3583ec3c401059569952184bd2e
naurias <naurias@protonmail.com>
2024-01-21 16:45:08 +0500

    xserver

--- configuration.nix
+++ configuration.nix
@@ -51,11 +51,11 @@
    };
 
   # Enable the X11 windowing system.
-#  services.xserver.enable = true;
+  services.xserver.enable = true;
 
 
   # Enable the Plasma 5 Desktop Environment.
-#  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
 #  services.xserver.desktopManager.plasma5.enable = true;
   
 
