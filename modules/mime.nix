0acedf0d2fdf3fbb0bc1f6930abf2be8c80ff5f2
naurias <naurias@protonmail.com>
2024-01-22 23:23:07 +0500

    mime fixes

--- modules/mime.nix
+++ modules/mime.nix
@@ -5,8 +5,8 @@
  xdg.mime.defaultApplications = {
 
     "inode/directory" = "thunar.desktop";
-    "image/png"  = "gwenview.desktop";
-    "image/jpeg"  = "gwenview.desktop";
+    "image/png"  = "org.kde.gwenview.desktop";
+    "image/jpeg"  = "org.kde.gwenview.desktop";
     "x-scheme-handler/http" = "brave-browser.desktop";
     "x-scheme-handler/https" = "brave-browser.desktop";
     "text/html" = "brave-browser.desktop";
@@ -14,7 +14,7 @@
     "video/mp4" = "mpv.desktop";
     "video/mpeg" = "mpv.desktop";
     "text/plain" = "nvim.desktop";
-    "application/pdf" = "okular.desktop";
+    "application/pdf" = "okularApplication_pdf.desktop";
 
   };
 
