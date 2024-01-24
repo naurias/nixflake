7cb45ed21592272e253238baa09b83c7e51b055f
naurias <naurias@protonmail.com>
2024-01-24 18:59:47 +0500

    added user group for naurias of libvirtd

--- configuration.nix
+++ configuration.nix
@@ -74,7 +74,7 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
    users.users.naurias = {
      isNormalUser = true;
-     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+     extraGroups = [ "wheel" "libvirtd" ]; # Enable ‘sudo’ for the user.
   #   packages = with pkgs; [
   #     firefox
   #     tree
