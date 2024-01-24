c0d67721ea5ce74018911d797bb7587433b4bd28
naurias <naurias@protonmail.com>
2024-01-24 21:20:17 +0500

    vbox

--- modules/virt.nix
+++ modules/virt.nix
@@ -6,4 +6,9 @@
 virtualisation.libvirtd.enable = true;
 programs.virt-manager.enable = true;
 
+
+   virtualisation.virtualbox.host.enable = true;
+   users.extraGroups.vboxusers.members = [ "naurias" ];
+
+
 }
