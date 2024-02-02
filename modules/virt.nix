8586e46aa5d245405c7646508bfdb798e3b185a1
naurias <naurias@protonmail.com>
2024-02-02 06:36:04 +0500

    removed podman

--- modules/virt.nix
+++ modules/virt.nix
@@ -10,17 +10,17 @@ programs.virt-manager.enable = true;
 #virtualisation.docker.enable = true;
 
 # Podman
-  virtualisation = {
-    podman = {
-      enable = true;
+#  virtualisation = {
+#    podman = {
+#      enable = true;
 
       # Create a `docker` alias for podman, to use it as a drop-in replacement
-      dockerCompat = true;
+#      dockerCompat = true;
 
       # Required for containers under podman-compose to be able to talk to each other.
-      defaultNetwork.settings.dns_enabled = true;
-    };
-  };
+#      defaultNetwork.settings.dns_enabled = true;
+#    };
+#  };
 #   virtualisation.virtualbox.host.enable = true;
 #   users.extraGroups.vboxusers.members = [ "naurias" ];
 
