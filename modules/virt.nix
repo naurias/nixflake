1a7d4e63da964f84a6620b9b9412141aecab38d4
naurias <naurias@protonmail.com>
2024-02-02 06:30:46 +0500

    testing podman

--- modules/virt.nix
+++ modules/virt.nix
@@ -9,6 +9,18 @@ programs.virt-manager.enable = true;
 # docker
 #virtualisation.docker.enable = true;
 
+# Podman
+  virtualisation = {
+    podman = {
+      enable = true;
+
+      # Create a `docker` alias for podman, to use it as a drop-in replacement
+      dockerCompat = true;
+
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.settings.dns_enabled = true;
+    };
+  };
 #   virtualisation.virtualbox.host.enable = true;
 #   users.extraGroups.vboxusers.members = [ "naurias" ];
 
