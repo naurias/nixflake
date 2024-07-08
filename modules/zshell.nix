fdb7122c3a58e4620d71cd20036a9042c99d3dda
naurias <nixre@proton.me>
2024-07-08 22:45:08 +0500

    zsh in module

--- modules/zshell.nix
+++ modules/zshell.nix
@@ -2,7 +2,17 @@
 
 {
 
-programs.zsh.enable = true;
+#programs.zsh.enable = true;
+
+programs.zsh = {
+  enable = true;
+  enableAutosuggestions = true;
+#  ohMyZsh.enable = true;
+#  ohMyZsh.plugins = [ "git" ];
+#  ohMyZsh.theme = "frisk";
+  syntaxHighlighting.enable = true;
+};
+
 users.users.nix.shell = pkgs.zsh;
 
 
